7cc34c1ba5c93906a76efb5c221b359dc2843a92
Luis Angulo <builmiguel@gmail.com>
2022-12-01 17:35:44 +0000

    S001 chore: initial resources

---
+++ server/index.js
@@ -0,0 +1,21 @@
+// Do this as the first thing so that any code reading it knows the right env.
+
+process.env.BABEL_ENV = "production";
+
+const express = require("express");
+const path = require("path");
+
+const PORT = 8080;
+const app = express();
+
+app.get("/product", (req, res) => {
+    res.send(["product1","product2"])
+});
+
+app.get("/profile", (req, res) => {
+    res.send(["profile1","profile2"])
+});
+
+app.listen(PORT, function () {
+  console.log(`Collection API Listening on port ${PORT}!\n`);
+});
