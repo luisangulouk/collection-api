dc9264804ff4da94c31769a7a92db35679e0fe49
Luis Angulo <builmiguel@gmail.com>
2022-12-06 15:22:07 +0000

    S001 feat: Add home route

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const path = require("path");
 const PORT = 8080;
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send(["home"])
+});
+
 app.get("/product", (req, res) => {
     res.send(["product1","product2"])
 });
